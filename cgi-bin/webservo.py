1802a49bda3f43c57692ebcde6787f1b60d92b2b
hayato0426 <hayato.matsumura2103@gmail.com>
2020-01-27 18:14:37 +0900

    first commit

--- cgi-bin/webservo.py
+++ cgi-bin/webservo.py
@@ -15,22 +15,21 @@ print('<input type="number" name="num" min="-90" max="90">')
 print('<input type="submit" name="sub" value="soushin">')
 print('</form>')
 
+form = cgi.FieldStorage()
+value = form.getvalue("sub")
+
+bottom = 2.5
+middle = 7.2
+top = 12.0
+
 def setservo(dig):
-    angle = (12.0 - 2.5) * (90 + dig) / 180 + 2.5
+    angle = (top - bottom) * (90 + dig) / 180 + bottom
     servo.ChangeDutyCycle(angle)
     time.sleep(1.0)
 
-
-def main():
-    form = cgi.FieldStorage()
-    value = form.getvalue("sub")
-
-    if value == "soushin":
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(2, GPIO.OUT)
-        servo = GPIO.PWM(2, 50)
-        servo.start(0.0)
-        setservo(int(form.getvalue("num")))
-
-if __name__ == "__main__":
-    main()
+if value == "soushin":
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(2, GPIO.OUT)
+    servo = GPIO.PWM(2, 50)
+    servo.start(0.0)
+    setservo(int(form.getvalue("num")))
